46a9e2efbc3d59162c729593bfc609ea62038c76
Ankit8830 <98680014+Ankit8830@users.noreply.github.com>
2023-02-16 18:50:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    String name = "Ankit Mishra";
+    String school = "Kishor Mangal Murti High School";
+    char grade = 'A';
+    char section = 'B';
+    int maths = 75;
+    int science = 70;
+    int english = 56;
+   
+    System.out.println(school);
+    System.out.println("Report card");
+    System.out.println("Name = "+name);
+    System.out.println("section = "+section);
+    System.out.println("grade = "+grade);
+    System.out.println("maths = "+maths);
+    System.out.println("science = "+science);
+    System.out.println("English = "+english);
+  
+   
+  }
+}
